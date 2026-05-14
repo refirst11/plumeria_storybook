c332d708ad0f14b793bf690c7c0e386e54ee2450
refirst11 <ocelot2828@gmail.com>
2026-05-15 01:13:06 +0900

    chore: update import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { plumeria } from "@plumeria/vite-plugin";
+import plumeria from "@plumeria/unplugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), plumeria()],
+  plugins: [react(), plumeria.vite()],
   optimizeDeps: {
     exclude: ["@plumeria/core"],
   },
